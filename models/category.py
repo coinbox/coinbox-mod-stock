cde7158c953829649506df218a785b7a698eacdf
jadkik <jadkik94@gmail.com>
2012-09-18 22:50:16 +0300

    Initial commit

---
+++ models/category.py
@@ -0,0 +1,27 @@
+import cbpos
+
+import cbpos.mod.base.models.common as common
+
+from sqlalchemy import func, Table, Column, Integer, String, Float, Boolean, MetaData, ForeignKey
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy.ext.hybrid import hybrid_property, hybrid_method, Comparator
+
+class Category(cbpos.database.Base, common.Item):
+    __tablename__ = 'categories'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255), nullable=False, unique=True)
+    parent_id = Column(Integer, ForeignKey('categories.id'))
+
+    parent = relationship("Category", backref="children", remote_side=[id])
+
+    @hybrid_property
+    def display(self):
+        return self.name
+    
+    @display.expression
+    def display(self):
+        return self.name
+
+    def __repr__(self):
+        return "<Category %s>" % (self.name,)
---
+++ models/diary.py
@@ -0,0 +1,39 @@
+import cbpos
+
+import cbpos.mod.base.models.common as common
+
+from sqlalchemy import func, Table, Column, Integer, String, Float, Boolean, Enum, DateTime, MetaData, ForeignKey
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy.ext.hybrid import hybrid_property, hybrid_method, Comparator
+
+class DiaryEntry(cbpos.database.Base, common.Item):
+    __tablename__ = 'stockdiary'
+
+    id = Column(Integer, primary_key=True)
+    operation = Column(Enum('in', 'out', 'edit'), nullable=False)
+    quantity = Column(Integer, nullable=True)
+    date = Column(DateTime, nullable=False, default=func.current_timestamp())
+    product_id = Column(Integer, ForeignKey('products.id'))
+    
+    product = relationship("Product", backref="diaryentries")
+
+    keys = ('operation', 'quantity', 'date', 'product')
+
+    def __init__(self, *args, **kwargs):
+        # Entry Date cannot be changed
+        if 'date' in kwargs:
+            del kwargs['date']
+        cbpos.database.Base.__init__(self, *args, **kwargs)
+
+    @hybrid_property
+    def display(self):
+        # TODO arrange the display function, is it unique?
+        return str(self.quantity)+self.operation+'/'+self.product.name
+    
+    @display.expression
+    def display(self):
+        return func.concat(self.quantity, self.operation, '/', self.product.name)
+
+    def __repr__(self):
+        return "<DiaryEntry %d %s of %s on %s>" % \
+               (self.quantity, self.operation, self.product, self.date)
